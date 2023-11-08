76ae4c9cc53657f15b5b88df43ec1f884c1fc8d5
jen67 <amakrigift2000@gmail.com>
2023-11-08 17:42:11 +0100

    done

---
+++ tip-calculator-app-main/src/js/main.js
@@ -0,0 +1,7 @@
+const getTip = document.querySelector('#custom');
+const Bill = document.querySelector('#billAmount');
+const nOfPeople = document.querySelector('#people');
+const btn = document.querySelector('#btn');
+const Validation = document.querySelector('#checker');
+
+
