6d4f69d80badafe0a2b5dbc14c3179b94c5aba4f
jen67 <amakrigift2000@gmail.com>
2023-11-24 22:46:10 +0100

    more codes

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -4,10 +4,5 @@
     const hiddenTexts = document.querySelectorAll(".card-body-item p"); 
 
 
-     hoverTexts.forEach((text, index) => {
-       text.addEventListener("click", () => {
-         rotateImgs[index].style.transform = "rotate(180deg)";
-         hiddenTexts[index].style.display = "block";
-       });
-     }); 
+     
 })(); 
