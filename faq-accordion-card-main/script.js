d72b530c38a46cecc2f8d336064102c885006dac
jen67 <amakrigift2000@gmail.com>
2023-11-28 18:54:05 +0100

    more codes

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,18 +1,18 @@
 (function () {
   const hoverTexts = document.querySelectorAll(".card-body-item h2");
   const rotateImgs = document.querySelectorAll(".card-body-item img");
-  const hiddenTexts = document.querySelectorAll(".card-body-item p");
+  const hiddenTexts = document.querySelectorAll(".card-body-item div");
+  const card = document.querySelector(".card");
 
   hoverTexts.forEach((text, index) => {
     text.addEventListener("click", () => {
-
       hiddenTexts.forEach((paragraph, i) => {
         if (i !== index) {
           paragraph.classList.remove("visible");
           rotateImgs[i].classList.remove("rotated");
           hoverTexts[i].classList.remove("clicked");
         }
-      }); 
+      });
 
       rotateImgs[index].classList.toggle("rotated");
       hiddenTexts[index].classList.toggle("visible");
@@ -20,4 +20,3 @@
     });
   });
 })();
- 
