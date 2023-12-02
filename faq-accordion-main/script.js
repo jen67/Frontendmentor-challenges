9a11af6d7d3d55c3b65b83a111065b6c5ac16497
jen67 <amakrigift2000@gmail.com>
2023-12-02 12:32:13 +0100

    added more codes

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,4 +1,5 @@
 let faqAccordion = document.querySelectorAll(".accordion-header h2");
+let faqAccordionIcon = document.querySelectorAll(".accordion-header img"); 
 let faqAccordionContent = document.querySelectorAll(".accordion-body"); 
 
 
