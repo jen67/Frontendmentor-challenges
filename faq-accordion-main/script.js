15484c541b43ef37fed8efe20c04a9912a522f52
jen67 <amakrigift2000@gmail.com>
2023-12-02 09:39:36 +0100

    added more codes

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -2,7 +2,7 @@ let faqAccordion = document.querySelectorAll(".accordion-header h2");
 
 faqAccordion.forEach((accordion) => {
     accordion.addEventListener("click", function () {
-        faqAccordion.style.backgroundColor = "#f5f695";
+       accordion.style.backgroundColor = "#f5f695";
     });
 
 });
