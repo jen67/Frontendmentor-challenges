8f0817001401ff711fb6aa9bd3bd0bac5d218a52
jen67 <amakrigift2000@gmail.com>
2023-12-02 09:20:35 +0100

    added more codes

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -2,4 +2,10 @@ const accordionH = document.querySelectorAll(".accordion-header h2");
 const accordionBody = document.querySelectorAll(".accordion-body");
 const plusImg = document.querySelectorAll(".accordion-header img");
 
+accordionH.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        accordionBody[index].classList.toggle("collapsoing"); 
+    });
+});
+
 
