e103bb465fd9927b76ad6bcb87c8b9de0ffc6850
jen67 <amakrigift2000@gmail.com>
2023-12-02 02:31:16 +0100

    more codes

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,10 +1,9 @@
 const accordionH = document.querySelectorAll('.accordion-header h2');
-const accordionBody = document.querySelectorAll('.accordion-body');
+const accordionBody = document.querySelectorAll(".accordion-body");
 const plusImg = document.querySelectorAll(".accordion-header img");
 
-accordionH.forEach((item, index) => {
+accordionH.forEach((item) => {
     item.addEventListener('click', () => {
-        plusImg[index].classList.toggle('rotate');
-        item.nextElementSibling.classList.toggle('active');
+        accordionBody[index].classList.toggle('collapsing'); 
     })
 }); 
