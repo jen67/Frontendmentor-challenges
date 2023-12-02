ed030cc32a2386000a6c3e2d881012946a6b4363
jen67 <amakrigift2000@gmail.com>
2023-12-02 12:54:20 +0100

    added more codes

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -4,19 +4,25 @@
   const faqAccordionIcon = document.querySelectorAll(".accordion-header img");
   const faqAccordionContent = document.querySelectorAll(".accordion-body");
 
-  faqAccordion.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      // Close all accordions
-      faqAccordionContent.forEach((content, contentIndex) => {
-        if (contentIndex !== index) {
-          content.classList.remove("collapsing");
-          faqAccordionIcon[contentIndex].classList.remove("collapsing-img");
-        }
-      });
-
-      // Toggle the clicked accordion
-      faqAccordionContent[index].classList.toggle("collapsing");
-      faqAccordionIcon[index].classList.toggle("collapsing-img");
+  const toggleAccordion = (index) => {
+    // Close all accordions
+    faqAccordionContent.forEach((content, contentIndex) => {
+      if (contentIndex !== index) {
+        content.classList.remove("collapsing");
+        faqAccordionIcon[contentIndex].classList.remove("collapsing-img");
+      }
     });
+
+    // Toggle the clicked accordion
+    faqAccordionContent[index].classList.toggle("collapsing");
+    faqAccordionIcon[index].classList.toggle("collapsing-img");
+  };
+
+  faqAccordion.forEach((item, index) => {
+    item.addEventListener("click", () => toggleAccordion(index));
+  });
+
+  faqAccordionIcon.forEach((item, index) => {
+    item.addEventListener("click", () => toggleAccordion(index));
   });
 })();
