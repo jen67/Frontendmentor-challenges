c398a46863ba60867ca295942ad3362aaf926155
jen67 <amakrigift2000@gmail.com>
2023-12-09 23:50:22 +0100

    added more codes

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -5,20 +5,6 @@
     
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-        const email = input.value
-        if (validateEmail(email)) {
-        error.innerHTML = ''
-            form.classList.remove('error')
-            input.src = 'images/icon-error.svg'
-        } else {
-        error.innerHTML = 'Please provide a valid email' 
-        form.classList.add('error')
-        }
-    })
-    
-    function validateEmail(email) {
-        const re = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-
-        return re.test(email)
-    }
+        
+    }); 
 })()
