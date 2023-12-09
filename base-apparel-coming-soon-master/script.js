da9e93a36477bf5c8c92da4c2c4f569b343e94b1
jen67 <amakrigift2000@gmail.com>
2023-12-10 00:13:00 +0100

    more codes

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,24 +1,27 @@
 (function () {
-    const form = document.querySelector('form')
-    const input = document.querySelector('input')
-    const error = document.querySelector('.error')
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        if (validateEmail) {
-            error.innerHTML = '';
-            input.classList.remove(errorImg);
-        }
-          else {
-            error.innerHTML = 'Please provide a valid email';
-            input.classList.add(errorImg);
-        }
-        
-        function validateEmail() {
-            const email = input.value;
-            const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-            return regex.test(email);
-        }
-        
-    }); 
-})()
+  const form = document.querySelector("form");
+  const input = document.querySelector("input");
+  const error = document.querySelector("#error-text");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (validateEmail()) {
+      error.innerHTML = "";
+      input.classList.remove("errorImg");
+    } else {
+      error.innerHTML = "Please provide a valid email";
+      input.classList.add("errorImg");
+    }
+  });
+
+  function validateEmail() {
+    const email = input.value.trim(); // Trim to remove leading and trailing spaces
+    if (email === "") {
+      // Check if the input is empty
+      return false;
+    }
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  }
+})();
