4c2fc63f83fb33dc0116f0480c6f5dd067cfad01
jen67 <amakrigift2000@gmail.com>
2023-12-09 23:44:40 +0100

    added more codes

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -16,7 +16,8 @@
     })
     
     function validateEmail(email) {
-        const re = /\S+@\S+\.\S+/
+        const re = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
         return re.test(email)
     }
 })()
