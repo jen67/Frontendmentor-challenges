c85931afa823ec9db3ae44bec55a037de9636779
jen67 <amakrigift2000@gmail.com>
2023-12-10 23:22:30 +0100

    added some codes

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -2,6 +2,8 @@ const form = document.querySelector("form");
 const email = document.querySelector("#email");
 const error = document.querySelector(".error-text");
 
-form.addEventListener(submit, event){
-    event.preventDefault();
-}
+form.addEventListener("submit", (e) => { 
+    e.preventDefault();
+
+});
+    
