2d6776d17901706e8b8adaad2e4c455d45a662c6
jen67 <amakrigift2000@gmail.com>
2023-12-11 00:34:46 +0100

    added some codes

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -5,7 +5,7 @@ const input = document.querySelector("input");
 
 form.addEventListener("submit", (e) => { 
     e.preventDefault();
-    if (input.value === "" || input.value !== RegExp) {
+    if (input.value === "" || input.value !== match( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
         error.innerHTML = "Please provide a valid email"; 
      }
 
