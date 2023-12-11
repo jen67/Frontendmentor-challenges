f30098e2bed0f705bbc7fb061ac590021ac366ca
jen67 <amakrigift2000@gmail.com>
2023-12-11 16:45:55 +0100

    added some codes

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -1,22 +1,30 @@
 const form = document.querySelector("form");
-const email = document.querySelector("#email");
 const error = document.querySelector("#error-text"); 
 const input = document.querySelector("#input");
 
+function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+}
+
+function showError(message) {
+    error.textContent = message;
+    input.classList.add("error-border");
+}
+
+function clearError() {
+    error.textContent = "";
+    input.classList.remove("error-border");
+    input.value = ""; 
+}
+
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (input.value === "") {
-    error.innerHTML = "Email cannot be empty";
-    input.classList.add("error-border");
-  } else if (regex.test(input.value) === false) {
-    error.innerHTML = "Please provide a valid email";
-    input.classList.add("error-border");
-    }
-    
-  else {
-      error.innerHTML = "";
-      input.classList.remove("error-border");
-      input.value = ""; 
+    e.preventDefault();
+    if (input.value === "") {
+        showError("Email cannot be empty");
+    } else if (!validateEmail(input.value)) {
+        showError("Please provide a valid email");
+    } else {
+        clearError();
     }
 });
