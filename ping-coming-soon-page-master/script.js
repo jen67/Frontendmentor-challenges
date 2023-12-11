b8102999c4b9c5218a5031dedfd1fedc2849f364
jen67 <amakrigift2000@gmail.com>
2023-12-11 16:12:18 +0100

    added some codes

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -6,13 +6,17 @@ const input = document.querySelector("#input");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (input.value === "") {
-        error.innerHTML = "Email cannot be empty";
-        input.classList.add("error-border");
-        elif (regex.test(input.value) === false) {
-            error.innerHTML = "Please provide a valid email";
-            input.classList.add("error-border");
-        }
-  }
-});
+  if (input.value === "") {
+    error.innerHTML = "Email cannot be empty";
+    input.classList.add("error-border");
+  } else if (regex.test(input.value) === false) {
+    error.innerHTML = "Please provide a valid email";
+    input.classList.add("error-border");
+    }
     
+  else {
+      error.innerHTML = "";
+      input.classList.remove("error-border");
+      input.value = ""; 
+    }
+});
