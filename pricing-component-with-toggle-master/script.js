2181f52dbfea85edb5fdd85501a31fb2770a77da
jen67 <amakrigift2000@gmail.com>
2023-12-12 21:18:35 +0100

    added more codes

--- pricing-component-with-toggle-master/script.js
+++ pricing-component-with-toggle-master/script.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   toggleSwitch.addEventListener("change", () => {
     if (toggleSwitch.checked) {
-      prices.forEach((price) => (price.textContent = "New price when checked"));
+        prices.forEach((price) => (price.textContent = "message"));
+        prices[0].textContent = "19.99";
+        prices[1].textContent = "24.99";
+        prices[2].textContent = "39.99";
     } else {
       prices.forEach(
         (price) => (price.textContent = "Original price when not checked")
