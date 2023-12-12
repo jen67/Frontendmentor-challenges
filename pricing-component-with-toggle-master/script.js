5d488737304b71e9d887b6496b1119ca248fcb83
jen67 <amakrigift2000@gmail.com>
2023-12-12 21:06:31 +0100

    added more codes

--- pricing-component-with-toggle-master/script.js
+++ pricing-component-with-toggle-master/script.js
@@ -1,15 +1,14 @@
-const toggleButton = document.querySelector(".switch-toggle");
-const price = document.querySelectorAll(".amount");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleSwitch = document.querySelector("#toggle-switch");
+  const prices = document.querySelectorAll(".price");
 
-
-toggleButton.addEventListener("change", () => { 
-    if (toggleButton.checked) {
-        price[0].innerHTML = "$199.99";
-        price[1].innerHTML = "$249.99";
-        price[2].innerHTML = "$399.99";
+  toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+      prices.forEach((price) => (price.textContent = "New price when checked"));
     } else {
-        price[0].innerHTML = "$19.99";
-        price[1].innerHTML = "$24.99";
-        price[2].innerHTML = "$39.99";
+      prices.forEach(
+        (price) => (price.textContent = "Original price when not checked")
+      );
     }
+  });
 });
