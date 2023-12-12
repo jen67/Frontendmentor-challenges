ae9c43145862dcb8b0da710ae704e738162446a0
jen67 <amakrigift2000@gmail.com>
2023-12-12 20:58:52 +0100

    more codes

--- pricing-component-with-toggle-master/script.js
+++ pricing-component-with-toggle-master/script.js
@@ -1,2 +1,15 @@
 const toggleButton = document.querySelector(".switch-toggle");
 const price = document.querySelectorAll(".price");
+
+
+toggleButton.addEventListener("click", () => { 
+    if (toggleButton.checked) {
+        price[0].innerHTML = "$199.99";
+        price[1].innerHTML = "$249.99";
+        price[2].innerHTML = "$399.99";
+    } else {
+        price[0].innerHTML = "$19.99";
+        price[1].innerHTML = "$24.99";
+        price[2].innerHTML = "$39.99";
+    }
+});
