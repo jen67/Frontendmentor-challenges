e5c070ad43f0b0b2bde8f5fa8a46df2eef2a0505
jen67 <amakrigift2000@gmail.com>
2024-01-04 19:20:31 +0100

    done

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,4 +1,29 @@
 const adviceId = document.querySelector("#adviceId");
 const adviceText = document.querySelector("#adviceText");
 const adviceBtn = document.querySelector("#adviceBtn");
+const apiUrl = "https://api.adviceslip.com/advice";
+
+
+function getAdvice() {
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          console.error("error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        adviceId.textContent = `#${data.slip.id}`;
+        adviceText.textContent = `"${data.slip.advice}"`;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+}
+
+adviceBtn.addEventListener("click", getAdvice); 
+
 
