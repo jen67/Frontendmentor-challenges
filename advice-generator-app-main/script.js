bf576f634a9d4414b0b1d21e7dd289da0947770a
jen67 <amakrigift2000@gmail.com>
2024-01-04 19:21:40 +0100

    added some codes

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -14,7 +14,6 @@ function getAdvice() {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
         adviceId.textContent = `#${data.slip.id}`;
         adviceText.textContent = `"${data.slip.advice}"`;
       })
