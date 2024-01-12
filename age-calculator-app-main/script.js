b36b91923573cad6d6608d96f2ac75e901bee102
jen67 <amakrigift2000@gmail.com>
2024-01-12 15:37:57 +0100

    done

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector(".hr-line div");
+const button = document.querySelector(".btn");
 const input = document.querySelectorAll("input");
 const birthDay = document.querySelector("#day");
 const birthMonth = document.querySelector("#month");
@@ -6,8 +6,26 @@ const birthYear = document.querySelector("#year");
 const years = document.querySelector(".years");
 const months = document.querySelector(".months");
 const days = document.querySelector(".days");
-const erroText = document.querySelectorAll(".hidden-text");
+const erroTextYear = document.querySelector(".error-text-year");
+const erroTextMonth = document.querySelector(".error-text-month");
+const erroTextDay = document.querySelector(".error-text-day");
 
-button.addEventListener("click", () => {
+function checkAge() {
+    const day = parseInt(birthDay.value, 10);
+    const month = parseInt(birthMonth.value, 10);
+    const year = parseInt(birthYear.value, 10);
+
+    const currentDate = new Date()
+    const birthDate = new Date(year, month - 1, day);
+
+    if (birthDate > currentDate) {
+        erroTextYear.innerContent = "Must be in the past";
+        erroTextMonth.innerContent = "Must be a valid month";
+        erroTextDay.innerContent = "Must be a valid day";
+    }
     
-});
+};
+
+button.addEventListener("click", checkAge);
+
+
