972a85407d78801789d529d5a149eeff1d47694a
jen67 <amakrigift2000@gmail.com>
2024-01-12 14:52:32 +0100

    added more codes

---
+++ age-calculator-app-main/script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector(".hr-line div");
+const input = document.querySelectorAll("input");
+const birthDay = document.querySelector("#day");
+const birthMonth = document.querySelector("#month");
+const birthYear = document.querySelector("#year");
+const years = document.querySelector(".years");
+const months = document.querySelector(".months");
+const days = document.querySelector(".days");
+const erroText = document.querySelectorAll(".hidden-text");
+
+button.addEventListener("click", () => {
+    
+});
